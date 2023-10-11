da02d0a2785c2bb4dbbc1c5a2a334da6b87505d4
ajaykhgupta <ajaykhgupta@gmail.com>
2023-10-11 23:03:18 +0530

    update-product-apis and exceptions

--- inventory/controller/inventory_controller.py
+++ inventory/controller/inventory_controller.py
@@ -1,20 +1,12 @@
 from fastapi import APIRouter, Request, Depends
+from typing import List
+from inventory.handler.inventory_handler import ProductHandler
+from inventory.validators.common_validation import NotFoundException
 from sqlalchemy.orm import Session
-from inventory.models import Product
-from inventory.connection_registry import Base, SessionLocal, engine
-from inventory.product_schema import AddProduct
+from inventory.product_schema import AddProduct, PatchProduct, ProductSchema
+from inventory.connection_registry import get_db
 
 router = APIRouter()
-Base.metadata.create_all(bind=engine)
-
-
-# Dependency
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
 
 
 @router.get("/")
@@ -22,15 +14,37 @@ def root():
     return {"message": "hello world"}
 
 
-@router.get("/product", status_code=200)
-def get_product(db: Session = Depends(get_db)):
-    return db.query(Product).all()
+@router.get("/product", response_model=List[AddProduct], status_code=200)
+def get_products(db: Session = Depends(get_db), name: str = None, category: str = None):
+    products = ProductHandler().get_products(db, name, category)
+    if not products:
+        raise NotFoundException
+    return products
 
 
-@router.post("/product", status_code=200)
-def add_product(product_schema: AddProduct, db: Session = Depends(get_db)):
-    product = Product(**product_schema.dict())
-    db.add(product)
-    db.commit()
-    db.refresh(product)
+@router.get("/product/{product_id}", response_model=AddProduct, status_code=200)
+def get_product(product_id: int, db: Session = Depends(get_db)):
+    product = ProductHandler().get_product(db, product_id)
+    if not product:
+        raise NotFoundException
     return product
+
+
+@router.post("/product", response_model=AddProduct, status_code=200)
+def add_product(product_schema: AddProduct, db: Session = Depends(get_db)):
+    product = ProductHandler().add_product(product_schema, db)
+    return product
+
+
+@router.post("/product/bulk-upload", status_code=200)
+def bulk_upload_product(product_schema: List[ProductSchema], db: Session = Depends(get_db)):
+    bulk_product = ProductHandler().bulk_upload_product(product_schema, db)
+    if bulk_product:
+        return {"response": "Bulk products uploaded successfully!!"}
+
+
+@router.patch("/product/{product_id}", status_code=200)
+def patch_product(product_id: int, product_schema: PatchProduct, db: Session = Depends(get_db)):
+    product = ProductHandler().patch_product(product_id, product_schema, db)
+    if product:
+        return product
--- inventory/models.py
+++ inventory/models.py
@@ -7,7 +7,7 @@ class Product(Base):
     __tablename__ = "product"
 
     id = Column(Integer, primary_key=True, index=True)
-    name = Column(String, unique=True, index=True)
+    name = Column(String, unique=True, index=True, nullable=False)
     count = Column(Integer)
     category = Column(String)
     price = Column(Integer)
